fff245d98184398a3e5d6bc02f6692a838464136
bruno <bruno.lecouat@gmail.com>
2017-08-25 01:57:40 +0800

    fix generator collapse

--- gan.py
+++ gan.py
@@ -6,12 +6,12 @@ import shutil
 import os
 import conv_model
 
-max_iter = 5000
+max_iter = 20000
 batch_size = 256
 output_path = 'output'
 log_path = 'log'
-freq_print = 50
-freq_save = 200
+freq_print = 20
+freq_save = 1000
 KEEP_RATE = 0.7
 IM_HEIGHT = 28
 IM_WIDTH = 28
@@ -55,14 +55,14 @@ def train_gan():
 
     with tf.name_scope('generator_loss'):
         g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=dis_gen, labels=tf.ones_like(dis_gen)))
-        dis_logits_on_generated = tf.reduce_mean(dis_gen)
+        dis_logits_on_generated = tf.reduce_mean(tf.sigmoid(dis_gen))
     with tf.name_scope('discriminator_loss'):
         d_loss_real = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(logits=dis_data, labels=tf.ones_like(dis_data)))
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=dis_data, labels=tf.fill([batch_size,1],0.9)))
         d_loss_fake = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(logits=dis_gen, labels=tf.zeros_like(dis_gen)))
         d_loss = d_loss_fake + d_loss_real
-        dis_logits_on_real = tf.reduce_mean(dis_data)
+        dis_logits_on_real = tf.reduce_mean(tf.sigmoid(dis_data))
 
     optimzer = tf.train.AdamOptimizer(0.0001)
     # collecting 2 list of training variables corresponding to discriminator and generator
@@ -86,7 +86,7 @@ def train_gan():
     tf.summary.scalar('Discriminator_real_loss', d_loss_real)
     tf.summary.scalar('Discriminator_fake_loss', d_loss_fake)
     tf.summary.scalar('Discriminator_total_loss', d_loss)
-    tf.summary.scalar('logits_discriminator_on_real', dis_logits_on_real)
+    tf.summary.scalar('logits_discriminator_on_generated', dis_logits_on_generated)
     tf.summary.scalar('logits_discriminator_on_real', dis_logits_on_real)
 
     tf.summary.image('Generated_images', x_generated, 10)  # add 10 generated images to summary
@@ -112,14 +112,13 @@ def train_gan():
                 print("step %d" % (i))
 
             if i % freq_print == 0:
-                summary = sess.run(merged,feed_dict={x_data: x_batch, keep_pl: KEEP_RATE})
+                summary = sess.run(merged, feed_dict={x_data: x_batch, keep_pl: KEEP_RATE})
                 writer.add_summary(summary, i)
-            #
-            #     # saver.save(sess, os.path.join(output_path, "model"), global_step=global_step)
 
-            if i % freq_save ==0:
+            if i % freq_save == 0:
                 sample_images = sess.run(x_generated, feed_dict={x_data: x_batch, keep_pl: KEEP_RATE})
                 store_result(sample_images, os.path.join(output_path, "sample%s.jpg" % i))
+                #   saver.save(sess, os.path.join(output_path, "model"), global_step=global_step)
 
         im_g_sample = sess.run(x_generated)
         store_result(im_g_sample, os.path.join(output_path, "final_samples.jpg"))
